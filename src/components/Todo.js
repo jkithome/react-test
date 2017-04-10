cedf79cf17401e7faa31870730b9d51e1d0cae1a
Jeremy Kithome <jeremy.kithome@andela.com>
2017-04-10 19:29:26 +0300

    Replace edit todo modal

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,13 +3,12 @@ import moment from 'moment'
 import { Modal, Header, Actions } from 'semantic-ui-react'
 import {connect} from 'react-redux'
 import {editTodo, toggleTodo, deleteTodo} from '../redux/modules/todo/actions'
-// var pathToImages = require.context('../images', true);
 
 export class Todo extends Component {
   constructor() {
     super();
     this.state = {
-      modalOpen: false,
+      formOpen: false,
       todo: {}
     }
     this.handleOpen=this.handleOpen.bind(this);
@@ -21,11 +20,11 @@ export class Todo extends Component {
   }
 
   handleOpen(e) {
-    this.setState({modalOpen: true});
+    this.setState({formOpen: true});
   }
 
   handleClose(e) {
-    this.setState({modalOpen: false});
+    this.setState({formOpen: false});
     this.setState({todo: {}});
   }
 
@@ -55,56 +54,54 @@ export class Todo extends Component {
       <div className="column">
         <div className="ui brown card">
           <img className="ui image" src={this.props.url} />
-          <div className="content">
-            <div className="header">{this.props.title}</div>
-            <div className="meta">{this.props.project}</div>
-            <div className="meta">Created {moment(this.props.createdAt).fromNow()}</div>
-          </div>
-          <div className="extra content">
-            <div>
-              <div className="ui toggle checkbox" style={{marginBottom: '10px'}}>
-                <input type="checkbox" name="public" value="on" defaultChecked ={this.props.done} onChange={this.handleToggle}/>
-                <label>Complete</label>
+          {this.state.formOpen ?
+            <div className="content">
+              <div className='ui form'>
+                <div className='field'>
+                  <label>Title</label>
+                  <input type='text'
+                    name="title"
+                    defaultValue={this.props.title}
+                    onChange={this.handleFieldChange}
+                  />
+                </div>
+                <div className='field'>
+                  <label>Project</label>
+                  <input type='text'
+                    name="project"
+                    defaultValue={this.props.project}
+                    onChange={this.handleFieldChange}
+                  />
+                </div>
               </div>
-              <div className="ui two buttons">
-                <Modal
-                  trigger={<button className='ui basic green button' onClick={this.handleOpen}>Edit</button>}
-                  open={this.state.modalOpen}
-                  onClose={this.handleClose}
-                >
-                  <Modal.Header>Edit Todo</Modal.Header>
-                  <Modal.Content>
-                    <div className='ui form'>
-                      <div className='field'>
-                        <label>Title</label>
-                        <input type='text'
-                          name="title"
-                          defaultValue={this.props.title}
-                          onChange={this.handleFieldChange}
-                        />
-                      </div>
-                      <div className='field'>
-                        <label>Project</label>
-                        <input type='text'
-                          name="project"
-                          defaultValue={this.props.project}
-                          onChange={this.handleFieldChange}
-                        />
-                      </div>
-                    </div>
-                  </Modal.Content>
-                  <Modal.Actions>
-                    <button className='ui inverted green button' onClick={this.handleEdit}>
-                      <i className='checkmark icon'></i> Update
-                    </button>
-                    <button className='ui inverted red button' onClick={this.handleClose}>
-                      <i className='remove icon'></i> Cancel
-                    </button>
-                  </Modal.Actions>
-                </Modal>
-                <button className="ui red basic button" onClick={this.handleDelete}>Delete</button>
-              </div>
+            </div> :
+            <div className="content">
+              <div className="header">{this.props.title}</div>
+              <div className="meta">{this.props.project}</div>
+              <div className="meta">Created {moment(this.props.createdAt).fromNow()}</div>
             </div>
+          }
+          <div className="extra content">
+            {this.state.formOpen ?
+              <div className="ui two buttons">
+                <button className='ui basic green button' onClick={this.handleEdit}>
+                  <i className='checkmark icon'></i> Update
+                </button>
+                <button className='ui basic red button' onClick={this.handleClose}>
+                  <i className='remove icon'></i> Cancel
+                </button>
+              </div> :
+              <div>
+                <div className="ui toggle checkbox" style={{marginBottom: '10px'}}>
+                  <input type="checkbox" name="public" value="on" defaultChecked ={this.props.done} onChange={this.handleToggle}/>
+                  <label>Complete</label>
+                </div>
+                <div className="ui two buttons">
+                  <button className='ui basic green button' onClick={this.handleOpen}>Edit</button>
+                  <button className="ui basic red button" onClick={this.handleDelete}>Delete</button>
+                </div>
+              </div>
+            }
           </div>
         </div>
       </div>
