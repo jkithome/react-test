825ec09ea351c88f2f978221d9fcb759011ccb73
Jeremy Kithome <jeremy.kithome@andela.com>
2017-04-10 19:28:55 +0300

    Replace create todo modal

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -13,7 +13,7 @@ export class CreateTodo extends Component {
         title: null,
         project: null
       },
-      modalOpen: false
+      formOpen: false
     }
 
     this.handleOpen=this.handleOpen.bind(this);
@@ -23,12 +23,12 @@ export class CreateTodo extends Component {
   }
 
   handleOpen() {
-    this.setState({modalOpen: true});
+    this.setState({formOpen: true});
   }
 
   handleClose() {
     this.setState({todo: {title: null, project: null}});
-    this.setState({modalOpen: false});
+    this.setState({formOpen: false});
   }
 
   handleFieldChange(e) {
@@ -47,37 +47,54 @@ export class CreateTodo extends Component {
   render() {
     return (
       <div className="column" style={{textAlign: 'center'}}>
-        <Modal
-          trigger={
-            <div className="ui icon button" data-tooltip="Create New Todo" data-position="bottom center" onClick={this.handleOpen}>
-              <i className="add icon"></i>
-            </div>
-          }
-          open={this.state.modalOpen}
-          onClose={this.handleClose}
-        >
-          <Modal.Header>Create Todo</Modal.Header>
-          <Modal.Content>
-            <div className='ui form'>
-              <div className='field'>
-                <label>Title</label >
-                <input type='text' name="title" onChange={this.handleFieldChange} />
+        {this.state.formOpen ?
+          <div className="ui centered grid">
+            <div className="eight wide column" style={{boxShadow: '0 0 0 1px #d4d4d5, 0 2px 0 0 #a5673f, 0 1px 3px 0 #d4d4d5', margin: '20px 0', padding: 0}}>
+              <div className="header" style={{display: 'block',
+    fontFamily: 'Lato,"Helvetica Neue",Arial,Helvetica,sans-serif',
+    background: '#fff',
+    margin: 0,
+    padding: '1.25rem 1.5rem',
+    boxShadow: 'none',
+    color: 'rgba(0,0,0,.85)',
+    borderBottom: '1px solid rgba(34,36,38,.15)',fontSize: '1.42857143rem',
+    lineHeight: '1.28571429em',
+    fontWeight: 700}}>Create Todo</div>
+              <div className="content" style={{display: 'block',
+    width: '100%',
+    fontSize: '1em',
+    lineHeight: 1.4,
+    padding: '1.5rem',
+    background: '#fff'}}>
+                <div className='ui form'>
+                  <div className='field'>
+                    <label>Title</label >
+                    <input type='text' name="title" onChange={this.handleFieldChange} />
+                  </div>
+                  <div className='field'>
+                    <label>Project</label>
+                    <input type='text' name="project" onChange={this.handleFieldChange} />
+                  </div>
+                </div>
               </div>
-              <div className='field'>
-                <label>Project</label>
-                <input type='text' name="project" onChange={this.handleFieldChange} />
+              <div className="actions" style={{background: '#f9fafb',
+    padding: '1rem 1rem',
+    borderTop: '1px solid rgba(34,36,38,.15)',
+    textAlign: 'right'}}>
+                <button className='ui inverted green button' onClick={this.handleCreate}>
+                  <i className='checkmark icon'></i> Create
+                </button>
+                <button className='ui inverted red button' onClick={this.handleClose}>
+                  <i className='remove icon'></i> Cancel
+                </button>
               </div>
             </div>
-          </Modal.Content>
-          <Modal.Actions>
-            <button className='ui inverted green button' onClick={this.handleCreate}>
-              <i className='checkmark icon'></i> Create
-            </button>
-            <button className='ui inverted red button' onClick={this.handleClose}>
-              <i className='remove icon'></i> Cancel
-            </button>
-          </Modal.Actions>
-        </Modal>
+          </div>
+         :
+          <div className="ui icon button" data-tooltip="Create New Todo" data-position="bottom center" onClick={this.handleOpen}>
+            <i className="add icon"></i>
+          </div>
+        }
       </div>
     )
   }
