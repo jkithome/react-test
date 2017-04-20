c8e4e08de854f87132c5fa55ad27b0411c5928aa
Jeremy Kithome <jeremy.kithome@andela.com>
2017-04-20 15:35:20 +0300

    Remove unnecessary code and clear mocks

--- __tests__/todo-component-test.js
+++ __tests__/todo-component-test.js
@@ -53,56 +53,30 @@ describe('components', () => {
   describe('Shallow rendered Card', () => {
     it('should render self and subcomponents', () => {
       const { enzymeWrapper, props } = shallowSetup()
-
-      // console.log(enzymeWrapper.debug())
-
       expect(enzymeWrapper.find('img').hasClass('ui image')).toBe(true)
       expect(enzymeWrapper.find('.header').text()).toBe(props.title)
       expect(enzymeWrapper.find('.meta').first().text()).toBe(props.project)
       expect(enzymeWrapper.find('.meta').at(0).text()).toBe(props.project);
       expect(enzymeWrapper.find('.meta').at(1).text()).toBe(`Created ${moment(props.createdAt).fromNow()}`);
-
-      // const ModalProps = enzymeWrapper.find('Modal').props()
-      // expect(ModalProps.open).toBe(false)
-      // expect(typeof ModalProps.onClose).toBe('function')
-      // console.log(ModalProps.onClose);
-      // expect(ModalProps.)
-      // enzymeWrapper.find('button').forEach(function (node) {
-      //   expect(node.hasClass('ui button')).to.equal(true);
-      // });
-      // expect(enzymeWrapper.find('button').text()).toBe('Edit')
-      // Since it's a shallow render Modal Component isn't rendered so only 3 buttons should be present
       expect(enzymeWrapper.find('button.ui').length).toBe(2)
       expect(enzymeWrapper.find('button.ui.basic.red.button').text()).toBe('Delete')
       expect(enzymeWrapper.find('button.ui.basic.green.button').text()).toBe('Edit')
-      // expect(enzymeWrapper.find('button.ui.inverted.green.button').text()).toBe(' Update')
-      // expect(enzymeWrapper.find('button.ui.inverted.red.button').text()).toBe(' Cancel')
       expect(enzymeWrapper.containsMatchingElement(<button>Delete</button>)).toBe(true)
       expect(enzymeWrapper.containsMatchingElement(<button>Edit</button>)).toBe(true)
 
       const input = enzymeWrapper.find('input').first();
       expect(input.props().defaultChecked ).toBe(false);
-
-      // const todoInputProps = enzymeWrapper.find('TodoTextInput').props()
-      // expect(todoInputProps.newTodo).toBe(true)
-      // expect(todoInputProps.placeholder).toEqual('What needs to be done?')
     })
-
-    // it('should call addTodo if length of text is greater than 0', () => {
-    //   const { enzymeWrapper, props } = setup()
-    //   const input = enzymeWrapper.find('TodoTextInput')
-    //   input.props().onSave('')
-    //   expect(props.addTodo.mock.calls.length).toBe(0)
-    //   input.props().onSave('Use Redux')
-    //   expect(props.addTodo.mock.calls.length).toBe(1)
-    // })
   })
   describe('Mounted Card', () => {
     let wrapper, props_
     beforeEach(() => {
       const { enzymeWrapper, props } = mountSetup()
-      wrapper = enzymeWrapper
-      props_ = props
+      wrapper = enzymeWrapper;
+      props_ = props;
+      editTodo.mockClear();
+      toggleTodo.mockClear();
+      deleteTodo.mockClear();
     })
     it('should update the state property `formOpen`', () => {
       const button = wrapper.find('button').first();
